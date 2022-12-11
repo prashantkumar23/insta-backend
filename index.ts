ca377401e6d979d38ef2b9340c6164d8225278ab
Prashant Kumar <prashantdrew23@gmail.com>
2022-12-11 02:25:35 -0800

    new changes 23

--- index.ts
+++ index.ts
@@ -1,34 +1,40 @@
-require('dotenv').config()
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import express from 'express'
-import cookieParser from 'cookie-parser'
+require("dotenv").config();
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import express from "express";
+import cookieParser from "cookie-parser";
 
-import { AppModule } from './app.module';
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-  );
-  const appExpress = express()
+  const app = await NestFactory.create(AppModule);
+  const appExpress = express();
 
-  const PORT = process.env.PORT || 5000
+  const PORT = process.env.PORT || 5000;
   const options = new DocumentBuilder().build();
 
-
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup("docs", app, document);
 
-  if (process.env.NODE_ENV === 'production') {
-    appExpress.set('trust proxy', () => true); // trust first proxy
+  if (process.env.NODE_ENV === "production") {
+    appExpress.set("trust proxy", () => true); // trust first proxy
   }
-  app.use(cookieParser())
-  app.enableCors({ credentials: true, origin: true, methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', exposedHeaders: ["set-cookie"] })
+  app.use(cookieParser());
+  app.enableCors({
+    credentials: true,
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    exposedHeaders: ["set-cookie"],
+    allowedHeaders: [
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept",
+    ],
+  });
 
-  app.use(express.json({ limit: "5mb" }))
-  app.use(express.urlencoded({ extended: true, limit: "5mb" }))
+  app.use(express.json({ limit: "5mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "5mb" }));
   await app.listen(PORT);
   const url = await app.getUrl();
-  console.log(`App is running at ${url}`)
+  console.log(`App is running at ${url}`);
 }
 bootstrap();
