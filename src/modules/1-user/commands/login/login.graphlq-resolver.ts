bf58843bcad5ead38c47df9a0183fd021c16f74b
Prashant Kumar <prashantdrew23@gmail.com>
2022-12-11 04:19:30 -0800

    new changes 38

--- src/modules/1-user/commands/login/login.graphlq-resolver.ts
+++ src/modules/1-user/commands/login/login.graphlq-resolver.ts
@@ -1,73 +1,91 @@
-import { Args, Context, GqlExecutionContext, GraphQLExecutionContext, Mutation, Query, Resolver } from '@nestjs/graphql'
-import { CommandBus } from '@nestjs/cqrs'
-import { Res, UseGuards } from '@nestjs/common'
+import {
+  Args,
+  Context,
+  GqlExecutionContext,
+  GraphQLExecutionContext,
+  Mutation,
+  Query,
+  Resolver,
+} from "@nestjs/graphql";
+import { CommandBus } from "@nestjs/cqrs";
+import { Res, UseGuards } from "@nestjs/common";
 
-import { LoginCommand } from './login.command'
-import { LoginRequest } from './login.request.dto'
-import { LoginResponse } from '../../response/login.response.dto'
-import { CongnitoAuthGuard } from '../../auth.guard'
-import { User } from '../../user.type'
-import { CurrentUser } from '../../currentuser.decorator'
+import { LoginCommand } from "./login.command";
+import { LoginRequest } from "./login.request.dto";
+import { LoginResponse } from "../../response/login.response.dto";
+import { CongnitoAuthGuard } from "../../auth.guard";
+import { User } from "../../user.type";
+import { CurrentUser } from "../../currentuser.decorator";
 
 @Resolver()
 export class LoginGraphqlResolver {
-    constructor(private readonly commandBus: CommandBus,
-    ) { }
+  constructor(private readonly commandBus: CommandBus) {}
 
-    @Query(() => User, { nullable: true })
-    @UseGuards(CongnitoAuthGuard)
-    whoAmI(@CurrentUser() user: User) {
-        return user.userDetails;
+  @Query(() => User, { nullable: true })
+  @UseGuards(CongnitoAuthGuard)
+  whoAmI(@CurrentUser() user: User) {
+    return user.userDetails;
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Context() context: any,
+    @Args("input") input: LoginRequest,
+    @Res({ passthrough: true }) response: any
+  ): Promise<LoginResponse> {
+    try {
+      const command = new LoginCommand(input);
+      const response = await this.commandBus.execute(command);
+      const cookieOptions = {
+        sameSite: "None",
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production" ? true : false,
+        maxAge: response.AuthenticationResult.ExpiresIn * 1000, // 1 day,
+        path: "/graphql",
+        // domain: process.env.NODE_ENV === "production" ? "insta-frontend-gules.vercel.app" : "localhost"
+      };
+      // console.log("Cookie Options", cookieOptions)
+      // console.log("Cookie *******")
+      // context.res.cookie("test", "test")
+
+      context.res.cookie(
+        "Authorization",
+        response.AuthenticationResult.AccessToken,
+        cookieOptions
+      );
+      context.res.cookie(
+        "Idtoken",
+        response.AuthenticationResult.IdToken,
+        cookieOptions
+      );
+
+      context.res.header(
+        "Set-Cookie",
+        "mycookie=hello; Secure; HttpOnly; SameSite=None; Path=/; Max-Age=99999999;"
+      );
+      context.res.header(
+        "Set-Cookie",
+        `Authorization=${response.AuthenticationResult.AccessToken}; Secure=${
+          process.env.NODE_ENV === "production" ? true : false
+        }; HttpOnly; SameSite=None; Path=/; Max-age=${response.AuthenticationResult.ExpiresIn * 1000}`
+      );
+      context.res.header(
+        "Set-Cookie",
+        `Idtoken=${response.AuthenticationResult.IdToken}; Secure=${
+          process.env.NODE_ENV === "production" ? true : false
+        }; HttpOnly; SameSite=None; Path=/; Max-age=${response.AuthenticationResult.ExpiresIn * 1000}`
+      );
+
+    //   context.res.header("Idtoken", response.AuthenticationResult.IdToken);
+
+      // console.log("response", response)
+      return { message: "Login Successfull!", isSuccess: true };
+    } catch (err) {
+      console.log(err);
+      return { message: err.message, isSuccess: false };
     }
-
-    @Mutation(() => LoginResponse)
-    async login(
-        @Context() context: any,
-        @Args('input') input: LoginRequest,
-        @Res({ passthrough: true }) response: any
-    ): Promise<LoginResponse> {
-
-        try {
-            const command = new LoginCommand(input)
-            const response = await this.commandBus.execute(command)
-            const cookieOptions = {
-                sameSite: "None",
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production' ? true : false,
-                maxAge: response.AuthenticationResult.ExpiresIn * 1000, // 1 day,
-                path: "/graphql",
-                // domain: process.env.NODE_ENV === "production" ? "insta-frontend-gules.vercel.app" : "localhost"
-            }
-            // console.log("Cookie Options", cookieOptions)
-            // console.log("Cookie *******")
-            // context.res.cookie("test", "test")
-
-            context.res.cookie(
-                'Authorization',
-                response.AuthenticationResult.AccessToken, cookieOptions);
-            context.res.cookie(
-                'Idtoken',
-                response.AuthenticationResult.IdToken, cookieOptions);
-
-
-            context.res.header("Set-Cookie", "mycookie=hello; Secure; HttpOnly; SameSite=None; Path=/; Max-Age=99999999;")
-            context.res.header('Authorization',
-                response.AuthenticationResult.AccessToken)
-
-            context.res.header(
-                'Idtoken',
-                response.AuthenticationResult.IdToken);
-
-            // console.log("response", response)
-            return { message: "Login Successfull!", isSuccess: true }
-        } catch (err) {
-            console.log(err)
-            return { message: err.message, isSuccess: false }
-        }
-
-    }
+  }
 }
-
 
 /* 
 
